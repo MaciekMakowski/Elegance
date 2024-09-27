58ecd45bb92418f86ce48dec27447a43996f14f6
MaciekMakowski <mvciek.mvkowski@gmail.com>
2024-09-27 17:40:28 +0200

    Refactor Footer component to add full height to the justify-center container

--- Elegance/src/components/Footer.jsx
+++ Elegance/src/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
   return (
     <footer className="bg-dark py-[120px] flex justify-center" id="footer">
       <div className="flex w-full flex-col md:flex-row max-w-[1450px] justify-between items-center md:items-start gap-8">
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center h-full">
           <Logo className="h-32 w-32" />
           <h5 className="text-white font-primary text-6xl font-semibold">
             EL<span className="text-primary text-4xl">EGANCE</span>
