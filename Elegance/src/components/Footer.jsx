e8a99381595eef7d89e66dda35fe6617455a4c32
MaciekMakowski <mvciek.mvkowski@gmail.com>
2024-09-27 17:39:58 +0200

    Refactor CSS styles and add alignment to body and footer

--- Elegance/src/components/Footer.jsx
+++ Elegance/src/components/Footer.jsx
@@ -30,7 +30,7 @@ const services = [
 const Footer = () => {
   return (
     <footer className="bg-dark py-[120px] flex justify-center" id="footer">
-      <div className="flex w-full flex-col md:flex-row max-w-[1450px] justify-between items-center gap-8">
+      <div className="flex w-full flex-col md:flex-row max-w-[1450px] justify-between items-center md:items-start gap-8">
         <div className="flex flex-col justify-center">
           <Logo className="h-32 w-32" />
           <h5 className="text-white font-primary text-6xl font-semibold">
